20d33f11225db97c53db571fcac9a4a4ab55fe8c
Aditya Tetali <adityatetali@gmail.com>
2026-07-04 10:55:26 -0500

    Fix module block syntax in policy-testing.tf

--- policy-testing.tf
+++ policy-testing.tf
@@ -2,7 +2,6 @@
   module "policy" {
   source  = "app.terraform.io/adityatetaliorg/policy/azure"
   version = "0.0.1"
-}
   scope_id = "/subscriptions/941863f5-fa95-4a2e-b1bc-7b46fd5e0236"
 
   policy_definitions = {
